d379e9c8d6237e67716df843a23b913e38cfbe92
DanielM3388 <israelibit@gmail.com>
2019-03-22 15:22:11 +0200

    Added VisionAlign

---
+++ src/main/java/frc/actions/VisionAlign.java
@@ -0,0 +1,52 @@
+package frc.actions;
+
+import edu.flash3388.flashlib.math.Mathf;
+import edu.flash3388.flashlib.robot.Action;
+import frc.robot.Robot;
+
+public class VisionAlign extends Action {
+    private final double mMargin;
+
+    private double setpoint;
+    private double mRotateSpeed;
+
+    public VisionAlign(double margin, double rotateSpeed) {
+        requires(Robot.driveSystem);
+
+        mMargin = margin;
+        mRotateSpeed = rotateSpeed;
+    }
+
+    public VisionAlign(double margin) {
+        requires(Robot.driveSystem);
+
+        mMargin = margin;
+        mRotateSpeed = 0.2;
+    }
+
+    @Override
+    protected void initialize() {
+        Robot.driveSystem.resetGyro();
+        setpoint = Robot.driveSystem.getVisionAngle();
+    }
+
+    @Override
+    protected void end() {
+        Robot.driveSystem.stop();
+    }
+
+    @Override
+    protected void execute() {
+        Robot.driveSystem.tankDrive(mRotateSpeed * Math.signum(setpoint), -mRotateSpeed * Math.signum(setpoint));
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return inRotationThreshold();
+    }
+
+    private boolean inRotationThreshold() {
+        double current = Robot.driveSystem.rotatePID.getPIDSource().pidGet();
+        return Mathf.constrained(setpoint - current, -mMargin, mMargin);
+    }
+}
